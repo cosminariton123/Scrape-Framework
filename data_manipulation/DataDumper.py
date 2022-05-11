ab47bd6075ddcac42a3f43f6f0deca5ee32baed0
Darkrider123 <cosmin.ariton@gmail.com>
2022-05-11 14:38:50 +0300

    refactored output file name

--- data_manipulation/DataDumper.py
+++ data_manipulation/DataDumper.py
@@ -19,8 +19,8 @@ def save_results(results, result_path, input_file_name):
 
 def success_write_file(results, result_path, input_file_name):
     data_frame_ok = pandas.DataFrame(results)
-    data_frame_ok.to_excel(os.path.join(result_path , "Succsess " + input_file_name + ".xlsx"))
+    data_frame_ok.to_excel(os.path.join(result_path , "Succsess " + input_file_name.split))
 
 def error_write_file(results, result_path, input_file_name):
     data_frame_er = pandas.DataFrame(results)
-    data_frame_er.to_excel(os.path.join(result_path , "Error " + input_file_name + ".xlsx"))
+    data_frame_er.to_excel(os.path.join(result_path , "Error " + input_file_name))
