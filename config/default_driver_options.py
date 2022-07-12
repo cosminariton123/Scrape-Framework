fe48b5c8543db8a1cc9fd212fdec80aa54eeecbd
Darkrider123 <cosmin.ariton@gmail.com>
2022-07-12 12:56:17 +0300

    updated selenium deleted unsupported opera

--- config/default_driver_options.py
+++ config/default_driver_options.py
@@ -2,7 +2,6 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options as ChromeOptions
 from selenium.webdriver.firefox.options import Options as FireFoxOptions
 from selenium.webdriver.edge.options import Options as EdgeOptions
-from selenium.webdriver.opera.options import Options as OperaOptions
 
 default_chrome_options = ChromeOptions()
 default_chrome_options.add_argument("--start-maximized")
@@ -16,10 +15,6 @@ default_edge_options = EdgeOptions()
 default_firefox_options = FireFoxOptions()
 #default_firefox_options.add_argument("--start-maximized")
 #default_firefox_options.add_argument("--headless")
-
-default_opera_options = OperaOptions()
-#default_opera_options.add_argument("--start-maximized")
-#default_opera_options.add_argument("--headless")
 
 
 TIMEOUT = 15
--- core/drivers/driver_instance.py
+++ core/drivers/driver_instance.py
@@ -1,10 +1,8 @@
-from config.default_driver_options import default_chrome_options, default_edge_options, default_firefox_options, \
-    default_opera_options
+from config.default_driver_options import default_chrome_options, default_edge_options, default_firefox_options
 from config.paths import CHROMEDRIVERPATH, EDGEDRIVERPATH, FIREFOXDRIVERPATH, OPERADRIVERPATH, SAFARIDRIVERPATH
 from core.drivers.chrome import ChromeDriver
 from core.drivers.edge import EdgeDriver
 from core.drivers.firefox import FirefoxDriver
-from core.drivers.opera import OperaDriver
 
 
 def get_new_default_chrome_driver():
@@ -23,13 +21,6 @@ def get_new_default_firefox_driver():
     return FirefoxDriver(executable_path=FIREFOXDRIVERPATH,
                          options=default_firefox_options
                          )
-
-
-def get_new_default_opera_driver():
-    return OperaDriver(executable_path=OPERADRIVERPATH,
-                       options=default_opera_options
-                       )
-
 
 
 DRIVER = None
