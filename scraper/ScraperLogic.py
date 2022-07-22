10d54c46dc2bdbc591f0d037f38798e1a7092559
Darkrider123 <cosmin.ariton@gmail.com>
2022-07-22 14:33:43 +0300

    deleted some useless imports

--- scraper/ScraperLogic.py
+++ scraper/ScraperLogic.py
@@ -1,14 +1,3 @@
-import time
-
-from selenium.common.exceptions import TimeoutException
-
-from scraper.ScraperLogicUtils import delete_shadow_section_harting
-import core.drivers.driver_instance as driver_instance
-from core.element_type.textbox import TextBox
-from core.element_type.button import Button
-from core.element_type.table import Table
-from core.element_type.input_textbox import InputTextBox
-
 def scraper_logic(item):                #Aici e logica de scraper. Daca aveam un singur site, scriam direct aici
                                                #Cum avem 3, consideram functia drept interfata si implementam separat ca sa putem schimba
                                                 #Implementarea dupa nevoie
