85e2965c39830cee4a15a9c73c6ee92c37cd9613
Darkrider123 <cosmin.ariton@gmail.com>
2022-09-01 13:44:52 +0300

    refactored and fixed weidmueller implementation

--- config/other.py
+++ config/other.py
@@ -6,7 +6,7 @@ from simple_math_for_process_random_delay.mathematical_constants import INF
 INF = INF
 
 
-NR_OF_PROCESSES = 1
+NR_OF_PROCESSES = 10
 
 
 DELAY_BETWEEN_PROCESS_LAUNCHES = normal_distribution_extraction(2, 7)
--- scraper/implementations/weidmueller.py
+++ scraper/implementations/weidmueller.py
@@ -49,21 +49,15 @@ def scrape_accessories(item):
         accesories_products_button = Button(xpath = "//*[@id=\"accessoryList\"]", timeout = timeout_amount)
         accesories_products_button.click()
 
-        accesories_list_group = Table(xpath = "//*[@id=\"accessoryListDiv\"]", timeout = timeout_amount, table_children_timeout = 2)
-
         result = ""
-        for group in accesories_list_group.rows:   
-            accesories_group_products_table = Table(xpath = "/table/tbody/tr/td", parent=group, timeout = 0.1, table_children_timeout = 0.2)
-            accesories_group_products_table.rows = accesories_group_products_table.rows[1:]
 
-            for subgroup in accesories_group_products_table.rows:
-            
-                subgroup_wrapper = Table(xpath = "//div[@class=\"ProductInfoWrapper\"]", parent = subgroup, timeout = timeout_amount, table_children_timeout=0.1)
-    
-                for product in subgroup_wrapper.rows:
-                    part_number_textbox = TextBox(xpath = "//span[@class=\"listAttributeValue products.BaseProduct.bestNr\"]", parent = product, timeout = timeout_amount)
-                    result += part_number_textbox.get_text() + ","
+        all_pn_xpath = "//div[@id=\"accessoryListDiv\"]//span[@class=\"listAttributeValue products.BaseProduct.bestNr\"]"
+        number_of_elements = driver_instance.DRIVER.get_number_of_elements_invisible(xpath= all_pn_xpath, timeout = timeout_amount)
         
+        for i in range(1, number_of_elements + 1):
+            result_text_box = TextBox(xpath = f"({all_pn_xpath})[{i}]", timeout = timeout_amount)
+            result += result_text_box.get_text() + ","
+
         if result == "":
             result = "Error"
         else:
